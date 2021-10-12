b1d1bafc4b937d0686ee0d01b1f6adb85278c7c0
Thomas Tuffin <ttuffin@redhat.com>
2021-10-12 21:21:49 +0200

    Update handler to properly parse json data.

--- tests/utils/ws_server.py
+++ tests/utils/ws_server.py
@@ -1,24 +1,33 @@
-import asyncio
 import websockets
+import json
 from websockets import WebSocketServer
 from dataclasses import dataclass, field
 
 
+@dataclass()
 class TestWSServer:
-    _websocket: WebSocketServer
-    heater_data: dict = {"TempCurrent": 10.1, "TempDesired": 21,
-                         "RunState": 7, "PumpMin": 1.6, "PumpMax": 5.5,
-                         "PumpCal": 0.022, "PumpCount": 0, "LowVoltCutout": 11.5,
-                         "FanMin": 1680, "FanMax": 4500, "FrostOn": 0, "FrostRise": 5
-                         }
+    _websocket: WebSocketServer = field(default=False, init=False)
+    heater_data: dict = field(default_factory=dict, init=False)
+
+    def __post_init__(self):
+        self.heater_data: dict = {"TempCurrent": 10.1, "TempDesired": 21,
+                                  "RunState": 7, "PumpMin": 1.6,
+                                  "PumpMax": 5.5, "PumpCal": 0.022,
+                                  "PumpCount": 0, "LowVoltCutout": 11.5,
+                                  "FanMin": 1680, "FanMax": 4500,
+                                  "FrostOn": 0, "FrostRise": 5
+                                  }
 
     async def handler(self, websocket, path):
         async for message in websocket:
-            reply = f"Data recieved as:  {message}!"
-            await websocket.send(reply)
+            message = json.loads(message)
+            if type(message) == dict:
+                self.heater_data.update(message)
+                await websocket.send(str(self.heater_data))
 
     async def start_server(self):
-        self._websocket = await websockets.serve(self.handler, "localhost", 8889)
+        self._websocket = await websockets.serve(
+            self.handler, "localhost", 8889)
 
     async def stop_server(self):
         await self._websocket.close()
