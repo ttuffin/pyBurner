959f21702e2b28f684d80fbcaa9ee897f0b71347
ttuffin <ttuffin@redhat.com>
2022-02-13 17:08:28 +0100

    Implementing basic tests.

--- tests/utils/ws_server.py
+++ tests/utils/ws_server.py
@@ -5,15 +5,15 @@ from dataclasses import dataclass, field
 
 
 @dataclass()
-class TestWSServer:
+class WSServer:
     _websocket: WebSocketServer = field(default=False, init=False)
     heater_data: dict = field(default_factory=dict, init=False)
 
     def __post_init__(self):
-        self.heater_data: dict = {"TempCurrent": 10.1, "TempDesired": 21,
-                                  "RunState": 7, "PumpMin": 1.6,
+        self.heater_data: dict = {"TempCurrent": 10, "TempDesired": 21,
+                                  "RunState": 0, "PumpMin": 1.6,
                                   "PumpMax": 5.5, "PumpCal": 0.022,
-                                  "PumpCount": 0, "LowVoltCutout": 11.5,
+                                  "PumpCount": 7239, "LowVoltCutout": 11.5,
                                   "FanMin": 1680, "FanMax": 4500,
                                   "FrostOn": 0, "FrostRise": 5
                                   }
@@ -23,7 +23,9 @@ class TestWSServer:
             try:
                 message = json.loads(message)
                 if message == {"Refresh": 1}:
-                    await websocket.send(str(self.heater_data))
+                    await websocket.send(json.dumps(self.heater_data))
+                elif message == {"Run": 1}:
+                    self.heater_data.update({"RunState": 9})
                 else:
                     self.heater_data.update(message)
             except json.decoder.JSONDecodeError:
@@ -33,6 +35,8 @@ class TestWSServer:
     async def start_server(self):
         self._websocket = await websockets.serve(
             self.handler, "localhost", 8889)
+        return True
 
     async def stop_server(self):
-        await self._websocket.close()
+        self._websocket.close()
+        await self._websocket.wait_closed()
